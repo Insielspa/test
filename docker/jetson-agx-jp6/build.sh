3d7468061b77e04c9b9faecb72c2c618254a0c26
xcesco <xcesco@gmail.com>
2024-09-21 12:40:13 +0200

    @wip project setup

--- docker/jetson-agx-jp6/build.sh
+++ docker/jetson-agx-jp6/build.sh
@@ -14,6 +14,7 @@ DEPLOY=false
 APP_OVERRIDE=false
 BUILD_TYPE=""
 APP_DIR="$APP_PATH"
+ASSETS_DIR="..\assets"
 
 usage() {
     echo "Usage: $0 [options]"
@@ -22,7 +23,7 @@ usage() {
     echo "  --deploy                 Push the image to the registry"
     echo "  --dev                    Build the development image"
     echo "  --prod                   Build the production image"
-    echo "  --app=<path>            Specify the application directory path (required for production)"
+    echo "  --app=<path>             Specify the application directory path (required for production)"
     exit 1
 }
 
@@ -80,20 +81,23 @@ if [[ "$BUILD_TYPE" == "production" && -z "$APP_DIR" ]]; then
     exit 1
 fi
 
+# Creazione della cartella build all'interno della directory di lavoro
+BUILD_DIR="$DOCKER_BASE_DIR/build"
+
+# Controllo se la cartella build esiste già e, in caso affermativo, rimuoverla
+if [ -d "$BUILD_DIR" ]; then
+   echo "La cartella build esiste già. Verrà rimossa..."
+   rm -rf "$BUILD_DIR"
+fi
+# Visualizzazione del percorso assoluto della cartella build
+ABSOLUTE_BUILD_DIR=$(realpath "$BUILD_DIR")
+echo "Il percorso assoluto della cartella di build è: $ABSOLUTE_BUILD_DIR"
+
+echo "Copio la cartella assets in $BUILD_DIR/assets"
+cp -r "$ASSETS_DIR" "$BUILD_DIR/assets"
+
+mkdir -p "$BUILD_DIR"
 if [[ "$BUILD_TYPE" == "production" ]]; then
-    # Creazione della cartella build all'interno della directory di lavoro
-    BUILD_DIR="$DOCKER_BASE_DIR/build"
-    
-    # Controllo se la cartella build esiste già e, in caso affermativo, rimuoverla
-    if [ -d "$BUILD_DIR" ]; then
-       echo "La cartella build esiste già. Verrà rimossa..."
-       rm -rf "$BUILD_DIR"
-    fi
-    # Visualizzazione del percorso assoluto della cartella build
-    ABSOLUTE_BUILD_DIR=$(realpath "$BUILD_DIR")
-    echo "Il percorso assoluto della cartella di build è: $ABSOLUTE_BUILD_DIR"
-    
-    mkdir -p "$BUILD_DIR"
     echo "Copio la cartella $APP_DIR in $BUILD_DIR/app"
     cp -r "$APP_DIR" "$BUILD_DIR/app"
     # Calcolo e visualizzazione della dimensione della cartella app
@@ -122,11 +126,10 @@ docker build --tag xcesco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION \
     --build-arg GROUP_ID=$GROUP_ID \
     --target $BUILD_TYPE -f $DOCKER_BASE_DIR/Dockerfile --load $DOCKER_BASE_DIR
 
-if [[ "$BUILD_TYPE" == "production" ]]; then
-   # Rimozione della cartella build
-   echo "Rimozione della cartella temporanea: $BUILD_DIR"
-   rm -rf "$BUILD_DIR"
-fi
+ # Rimozione della cartella build
+ echo "Rimozione della cartella di build: $BUILD_DIR"
+ rm -rf "$BUILD_DIR"
+
 
 if [ "$DEPLOY" = true ]; then
     echo "Pushing the image to the registry"
