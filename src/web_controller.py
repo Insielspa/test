343337f6bc35c8ce582bf6066a49e3b812bbe291
xcesco <xcesco@gmail.com>
2024-10-04 11:29:30 +0200

    @wip #1

--- src/web_controller.py
+++ src/web_controller.py
@@ -5,12 +5,17 @@ from flask import Flask, request, abort
 
 from fvgvisionai.common.pid_file import read_pid_from_file, terminate_process
 
+RUN_MAIN_ENV_SH = "/app/run_main_env.sh"
+
+RUN_MAIN_SH = "/app/run_main.sh"
+
 app = Flask(__name__)
+
 
 @app.route('/start', methods=['GET'])
 def start_app():
     # Ottieni il parametro 'type' dalla richiesta
-    execution_type = request.args.get('type','env')
+    execution_type = request.args.get('type', 'env')
 
     # Verifica che il parametro 'type' sia valido
     if execution_type not in ['file', 'env']:
@@ -18,18 +23,22 @@ def start_app():
 
     # Esegui il comando in base al valore di 'type'
     if execution_type == 'file':
-        command = ["/app/run_main.sh"]
+        command = [RUN_MAIN_SH]
     elif execution_type == 'env':
-        command = ["/app/run_main_env.sh"]
+        command = [RUN_MAIN_ENV_SH]
 
-    # Esegui il comando per avviare l'app come processo in background
-    subprocess.Popen(command,  stdout=None,  # Non redirige l'output, lasciandolo su stdout
-        stderr=None,  # Non redirige gli errori, lasciandoli su stderr
-        close_fds=True ) # Chiude i file descriptor non necessari)
+    execute_fvgvision_ai(command)
 
     # Avvia il processo come demone, senza bloccare il processo principale
 
     return "App avviata con successo"
+
+
+def execute_fvgvision_ai(command=[RUN_MAIN_ENV_SH]):
+    # Esegui il comando per avviare l'app come processo in background
+    subprocess.Popen(command, stdout=None,  # Non redirige l'output, lasciandolo su stdout
+                     stderr=None,  # Non redirige gli errori, lasciandoli su stderr
+                     close_fds=True)  # Chiude i file descriptor non necessari)
 
 
 @app.route('/stop', methods=['GET'])
@@ -54,5 +63,7 @@ def verify_status():
     else:
         return "STOPPED"
 
+
 if __name__ == "__main__":
+    execute_fvgvision_ai()
     app.run(host='0.0.0.0', port=8081)
